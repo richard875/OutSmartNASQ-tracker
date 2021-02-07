860dbbd2d76313ad6d5fae2037f0c6773534a1c6
richard875 <richard_875@me.com>
2021-02-08 01:12:30 +1300

    removed static from tree

--- data/static/gmailData.ts
+++
@@ -1,8 +0,0 @@
-class GmailInformation {
-  public static Firstname: string = "OutSmartNSDQ";
-  public static Lastname: string = "AutoEmailSender";
-  public static emailAddress: string = "outsmartnsdqautosender@gmail.com";
-  public static emailPassword: string = '"9~DuN[N_#Ue!KH7F';
-}
-
-export default GmailInformation;
