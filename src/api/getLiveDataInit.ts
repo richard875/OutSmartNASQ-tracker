b4417ee0824fabe132642f432345650080543921
richard875 <richard_875@me.com>
2021-02-26 23:26:43 +1300

    Remove test code

--- src/api/getLiveDataInit.ts
+++ src/api/getLiveDataInit.ts
@@ -9,17 +9,3 @@ getRawStockAPI(
   currentMarketStatus.holidayStatus.marketOpen,
   currentMarketStatus.currentDateString
 );
-
-import fetch from "node-fetch";
-import { v4 as uuidv4 } from "uuid";
-
-const test = async () => {
-  const response = await fetch(
-    "https://www.etoro.com/sapi/userstats/risk/UserName/OutSmartNSDQ/Contribution?client_request_id=56580be7-4dcf-4e2c-8964-c3eced0725bb"
-  );
-  const rawStockAPI = await response.text();
-
-  console.log(rawStockAPI);
-};
-
-test();
